c26d7611987f35c9560ad07f36a62b0fcb1ef386
reinahandal <handalreina@gmail.com>
2022-09-09 16:27:10 +0300

    add interactivity using JS

---
+++ script.js
@@ -0,0 +1,21 @@
+function islandLake(element) {
+    element.src = "media/island lake 2.png";
+}
+function islandLake2(element) {
+    element.src = "media/island lake 1.png";
+}
+function scale(element, value) {
+    element.style.transform = "scale(" + value + ")";
+}
+function hover(element) {
+    element.style.textShadow= "1px 1px 8px #b956c3";
+}
+function hoverOut(element) {
+    element.style.textShadow= "none";
+}
+function changeName(element) {
+    element.innerText = "رينا حنضل";
+    element.style.fontFamily = "'Kufam', sans-serif";
+    element.style.fontWeight = "100";
+    element.style.fontSize = "2.5rem";
+}
